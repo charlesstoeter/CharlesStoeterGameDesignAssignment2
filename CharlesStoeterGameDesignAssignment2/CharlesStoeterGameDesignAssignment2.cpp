7c4ebc0954b234ef6c5475352ec4ec93ac895e6d
Charles Stoeter <charlesstoeter@gmail.com>
2025-06-01 10:50:46 -0400

    started with initializations and and header class for gameboard

--- CharlesStoeterGameDesignAssignment2/CharlesStoeterGameDesignAssignment2.cpp
+++ CharlesStoeterGameDesignAssignment2/CharlesStoeterGameDesignAssignment2.cpp
@@ -1,10 +1,44 @@
 // CharlesStoeterGameDesignAssignment2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
 #include <iostream>
+
+//global x and global y for mouse clicks
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	if (!al_init()) {
+		std::cerr << "Failed to initialize Allegro!" << std::endl;
+		return -1;
+	}
+	if (!al_init_font_addon()) {
+		std::cerr << "Failed to initialize font addon!" << std::endl;
+		return -1;
+	}
+	if (!al_init_ttf_addon()) {
+		std::cerr << "Failed to initialize TTF addon!" << std::endl;
+		return -1;
+	}
+	if (!al_init_primitives_addon()) {
+		std::cerr << "Failed to initialize primitives addon!" << std::endl;
+		return -1;
+	}
+
+
+
+
+
+
+
+	// Your game logic here
+	al_shutdown_primitives_addon();
+	al_shutdown_ttf_addon();
+	al_shutdown_font_addon();
+	al_uninstall_system();
+	return 0;
+
 }
 
